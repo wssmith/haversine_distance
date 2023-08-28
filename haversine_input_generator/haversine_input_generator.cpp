61099f5c9fda18f694bf57af32bf69ce1caa69b8
Wesley Smith <WesleySmith163@gmail.com>
2023-08-27 20:59:55 -0700

    generate uniformly distributed points on the globe

--- haversine_input_generator/haversine_input_generator.cpp
+++ haversine_input_generator/haversine_input_generator.cpp
@@ -1,19 +1,96 @@
-#include <cstddef>
+#include <cstdlib>
 #include <exception>
+#include <fstream>
 #include <iostream>
+#include <vector>
 
 #include "random_generator.hpp"
 
 namespace
 {
+    struct globe_point
+    {
+        double x{};
+        double y{};
+    };
 
+    struct globe_point_pair
+    {
+        globe_point point1{};
+        globe_point point2{};
+    };
+
+    void write_point(std::ofstream& output_stream, globe_point point)
+    {
+        output_stream << R"({ "lat": )" << point.y << R"(, "lon": )" << point.x << R"( })";
+    }
+
+    void write_point_pair(std::ofstream& output_stream, const globe_point_pair& point_pair)
+    {
+        output_stream << R"({ "p1": )";
+        write_point(output_stream, point_pair.point1);
+        output_stream << R"(, "p2": )";
+        write_point(output_stream, point_pair.point2);
+        output_stream << " }";
+    }
+
+    void save_haversine_json(const char* path, const std::vector<globe_point_pair>& data)
+    {
+        std::ofstream output_stream { path, std::ios::out };
+
+        output_stream << "{\n  \"coordinates\": [\n";
+
+        if (!data.empty())
+        {
+            output_stream << "    ";
+            write_point_pair(output_stream, data[0]);
+
+            for (size_t i = 1; i < data.size(); ++i)
+            {
+                output_stream << ",\n    ";
+                write_point_pair(output_stream, data[i]);
+            }
+        }
+
+        output_stream << "\n  ]\n}\n";
+
+        output_stream.close();
+    }
 }
 
 int main()
 {
     try
     {
-        std::cout << "Hello World!\n";
+        // generate pairs of coordinates
+        constexpr double x_min = 0.0;
+        constexpr double x_max = 180.0;
+        constexpr double y_min = 0.0;
+        constexpr double y_max = 90.0;
+        constexpr int pair_count = 100;
+
+        uniform_real_generator x_rand{ x_min, x_max };
+        uniform_real_generator y_rand{ y_min, y_max };
+
+        std::vector<globe_point_pair> points{};
+        points.reserve(pair_count);
+
+        for (int i = 0; i < pair_count; ++i)
+        {
+            auto point_pair = globe_point_pair
+            {
+                .point1 = { .x = x_rand(), .y = y_rand() },
+                .point2 = { .x = x_rand(), .y = y_rand() }
+            };
+
+            points.push_back(point_pair);
+        }
+
+        // save coordinates to a json file
+        constexpr auto data_filename = "coordinates.json";
+        save_haversine_json(data_filename, points);
+
+        std::cout << "Saved memory to '" << data_filename << "'.\n";
     }
     catch (std::exception& ex)
     {
--- haversine_input_generator/random_generator.hpp
+++ haversine_input_generator/random_generator.hpp
@@ -10,73 +10,67 @@ template<typename Distribution, typename Engine = std::mt19937>
 class random_generator
 {
 public:
-	using result_type = typename Distribution::result_type;
-	using seed_type = unsigned int; // same as std::random_device::result_type
+    using result_type = typename Distribution::result_type;
+    using seed_type = unsigned int; // same as std::random_device::result_type
 
-	template<
-		typename... Params,
-		typename = std::enable_if_t<
-		(... && std::is_arithmetic_v<std::decay_t<Params>>)
-		&& !std::is_base_of_v<std::discrete_distribution<>, std::decay_t<Distribution>>
-		>
-	>
-	explicit random_generator(Params&&... args)
-	{
-		std::random_device dev;
-		_impl = std::make_unique<random_generator_impl>(Engine{ dev() }, std::forward<Params>(args)...);
-	}
+    template<typename... Params, typename = std::enable_if_t<(... && std::is_arithmetic_v<std::decay_t<Params>>)>>
+    explicit random_generator(Params... args)
+    {
+        std::random_device seed_generator;
+        _impl = std::make_unique<random_generator_impl>(Engine{ seed_generator() }, args...);
+    }
 
-	template<
-		typename UnaryOperation,
-		typename = std::enable_if<std::is_base_of_v<std::discrete_distribution<>, std::decay_t<Distribution>>>
-	>
-	random_generator(size_t nw, double x_min, double x_max, UnaryOperation fw)
-	{
-		std::random_device dev;
-		_impl = std::make_unique<random_generator_impl>(Engine{ dev() }, nw, x_min, x_max, fw);
-	}
+    template<typename UnaryOperation, typename = std::enable_if_t<std::is_base_of_v<std::discrete_distribution<>, std::decay_t<Distribution>>>>
+    random_generator(size_t nw, double x_min, double x_max, UnaryOperation fw)
+    {
+        std::random_device seed_generator;
+        _impl = std::make_unique<random_generator_impl>(Engine{ seed_generator() }, nw, x_min, x_max, fw);
+    }
 
-	~random_generator() = default;
-	random_generator(random_generator&& rhs) noexcept = default;
-	random_generator& operator=(random_generator&& rhs) noexcept = default;
-	random_generator(const random_generator& rhs) = delete;
-	random_generator& operator=(const random_generator& rhs) = delete;
+    ~random_generator() = default;
+    random_generator(random_generator&& rhs) noexcept = default;
+    random_generator& operator=(random_generator&& rhs) noexcept = default;
+    random_generator(const random_generator& rhs) = delete;
+    random_generator& operator=(const random_generator& rhs) = delete;
 
-	result_type operator()()
-	{
-		return _impl->draw_sample();
-	}
+    result_type operator()()
+    {
+        return _impl->draw_sample();
+    }
 
-	void seed(seed_type s)
-	{
-		_impl->seed(s);
-	}
+    void seed(seed_type s)
+    {
+        _impl->seed(s);
+    }
 
 private:
-	struct random_generator_impl
-	{
-		template<typename... Params>
-		explicit random_generator_impl(Engine&& engine, Params&&... args)
-			: _dist{ std::forward<Params>(args)... }, _engine{ std::forward<Engine>(engine) }
-		{
-		}
+    struct random_generator_impl
+    {
+        template<typename... Params>
+        explicit random_generator_impl(Engine&& engine, Params&&... args)
+            : _dist{ std::forward<Params>(args)... }, _engine{ std::forward<Engine>(engine) }
+        {
+        }
 
-		void seed(const seed_type s)
-		{
-			_engine.seed(s);
-		}
+        void seed(const seed_type s)
+        {
+            _engine.seed(s);
+        }
 
-		result_type draw_sample()
-		{
-			return _dist(_engine);
-		}
+        result_type draw_sample()
+        {
+            return _dist(_engine);
+        }
 
-	private:
-		Distribution _dist;
-		Engine _engine;
-	};
+    private:
+        Distribution _dist;
+        Engine _engine;
+    };
 
-	std::unique_ptr<random_generator_impl> _impl;
+    std::unique_ptr<random_generator_impl> _impl;
 };
+
+template<typename TResult = double>
+using uniform_real_generator = random_generator<std::uniform_real_distribution<TResult>>;
 
 #endif
