f300c384eddf38961b45cf7f36192bdb5c1e20fd
Wesley Smith <kyrandian@gmail.com>
2023-08-27 17:32:33 -0700

    add random number generator

--- haversine_input_generator/haversine_input_generator.cpp
+++ haversine_input_generator/haversine_input_generator.cpp
@@ -1,6 +1,30 @@
+#include <cstddef>
+#include <exception>
 #include <iostream>
+
+#include "random_generator.hpp"
+
+namespace
+{
+
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    try
+    {
+        std::cout << "Hello World!\n";
+    }
+    catch (std::exception& ex)
+    {
+        std::cout << "ERROR!! " << ex.what() << '\n';
+        return EXIT_FAILURE;
+    }
+    catch (...)
+    {
+        std::cout << "UNKNOWN ERROR!!\n";
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
