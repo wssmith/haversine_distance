bea8e050c565680347c40dba0fed0536b5c769ce
Wesley Smith <WesleySmith163@gmail.com>
2024-02-11 14:16:46 -0800

    add reference haversince implementation to processor project. rename project and solution.

---
+++ haversine_basic/haversine_formula.cpp
@@ -0,0 +1,31 @@
+#include "haversine_formula.hpp"
+
+#include <cmath>
+#include <numbers>
+
+namespace
+{
+    double square(double x)
+    {
+        return x * x;
+    }
+
+    double radians_from_degrees(double degrees)
+    {
+        constexpr double factor = std::numbers::pi / 180.0;
+        return factor * degrees;
+    }
+}
+
+double reference_haversine(double x0, double y0, double x1, double y1, double earth_radius)
+{
+    const double d_lat = radians_from_degrees(y1 - y0);
+    const double d_lon = radians_from_degrees(x1 - x0);
+    const double lat1 = radians_from_degrees(y0);
+    const double lat2 = radians_from_degrees(y1);
+
+    const double a = square(std::sin(d_lat / 2.0)) + std::cos(lat1) * std::cos(lat2) * square(std::sin(d_lon / 2.0));
+    const double c = 2.0 * std::asin(std::sqrt(a));
+
+    return earth_radius * c;
+}
---
+++ haversine_basic/haversine_formula.hpp
@@ -0,0 +1,8 @@
+﻿#ifndef WS_HAVERSINEFORMULA_HPP
+#define WS_HAVERSINEFORMULA_HPP
+
+inline constexpr double default_earth_radius = 6372.8;
+
+double reference_haversine(double x0, double y0, double x1, double y1, double earth_radius = default_earth_radius);
+
+#endif
