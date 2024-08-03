7d68120380b765c42870240431aed0af40314703
Wesley Smith <Wesley.Smith@smarsh.com>
2024-08-03 16:52:05 -0700

    minor formatting change

--- haversine_processor/profiler.cpp
+++ haversine_processor/profiler.cpp
@@ -40,7 +40,7 @@ namespace
     {
         const auto& anchors = profiler::get_anchors();
 
-        std::cout << "Performance profiles:\n";
+        std::cout << "\nPerformance profiles:\n";
 
         for (const profile_anchor& anchor : anchors)
         {
@@ -63,7 +63,7 @@ void profiler::print_results()
     const uint64_t overall_duration = get_overall_duration();
 
     const double overall_duration_ms = 1000.0 * overall_duration / cpu_freq;
-    std::cout << std::format(std::locale("en_US"), "Total time: {:.4f} ms (CPU freq {:Ld})\n\n", overall_duration_ms, cpu_freq);
+    std::cout << std::format(std::locale("en_US"), "Total time: {:.4f} ms (CPU freq {:Ld})\n", overall_duration_ms, cpu_freq);
 
     PRINT_ANCHORS(cpu_freq, overall_duration);
 }
